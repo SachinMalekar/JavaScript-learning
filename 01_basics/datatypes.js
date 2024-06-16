1c31e16208601de8a61fd44ef38265fecdaa00d2
SachinMalekar <55599572+SachinMalekar@users.noreply.github.com>
2024-06-16 14:34:56 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"
+console.log("sachin");
+let name = "sachin"
+let age = 28;
+let isLoggedIn = true;
+
+let state = "maharashtra"
+console.table([name,age,isLoggedIn,state])
+
+// number => 2 to power 53
+//  bigint 
+// string
+// boolean
+//  undefined
+// symbol   => unique
+// object
+
+console.log(typeof "sach")
+console.log(typeof null);
+console.log(typeof undefined);

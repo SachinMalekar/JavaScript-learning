dabe92cee214afc4fc7d70fb3ba76e24b37e91a1
SachinMalekar <55599572+SachinMalekar@users.noreply.github.com>
2024-06-16 13:37:57 +0000

    variable topic

---
+++ 01_basics/variable.js
@@ -0,0 +1,13 @@
+"use strict"
+const accountId = 12345
+let accountEmail = "sachin@gmail.com"
+var accountPass = "sach@123"
+accountCity = "Pune"
+const a = 10;
+console.log(a);
+// accountId = 10;  not allowed to change
+accountEmail = "hc@hc.com";
+accountPass = "hc@123"
+accountCity = "bengaluru"
+let accountState
+console.table([accountEmail, accountPass, accountCity, accountState]);
